9922c99d026d219560ff8118d40b7483b9b9caff
Jasima yasmin <jasimayasmin78@gmail.com>
2026-02-20 23:33:18 +0530

    add solutions

--- src/app/sales/page.tsx
+++ src/app/sales/page.tsx
@@ -1,13 +1,7 @@
-import Hero from '@/components/sales/Hero';
-import Feature from '@/components/sales/Feature';
-import Services from '@/components/sales/Services';
-import Process from '@/components/sales/Process';
-import Faq from '@/components/sales/Faq';
-import Integration from '@/components/sales/Integration';
-import Number from '@/components/sales/Number';
-import Team from '@/components/sales/Team';
+import SalesHero from '@/components/sales/SalesHero';
+import SalesDetails from '@/components/sales/SalesDetails';
+import SalesTestimonial from '@/components/sales/SalesTestimonial';
 import CTA from '@/components/shared/cta/CTA';
-import Blog from '@/components/sales/Blog';
 import { defaultMetadata } from '@/utils/generateMetaData';
 import { Metadata } from 'next';
 
@@ -19,25 +13,20 @@ export const metadata: Metadata = {
 const SalesPage = () => {
   return (
     <main className="bg-background-2 dark:bg-background-5">
-      <Hero />
-      <Feature />
-      <Services />
-      <Process />
-      <Faq />
-      <Integration />
-      <Number />
-      <Team />
+      <SalesHero />
+      <SalesDetails />
+      <SalesTestimonial />
       <CTA
         className="dark:bg-background-6 bg-white py-[50px] md:py-[70px] lg:py-[85px] xl:py-[100px]"
+        badgeText="Start Selling"
+        badgeClass="!badge-green"
         ctaHeading="Build a complete sales system with smart assistance"
         description="Start your journey with Caartzee and see your ideas come to life easily and creatively."
-        ctaBtnText="Start Selling"
+        ctaBtnText="Start Selling Smarter"
         btnClass="hover:btn-secondary dark:hover:btn-accent"
       />
-      <Blog />
     </main>
   );
 };
 
-SalesPage.displayName = 'SalesPage';
 export default SalesPage;
