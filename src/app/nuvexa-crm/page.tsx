0b4e41fdf46b8929b20c291d9a37cab1521146bc
akhil_jinu <akhil_jinu@edmondremedis.com>
2026-02-25 19:49:56 +0530

    Component

--- src/app/nuvexa-crm/page.tsx
+++ src/app/nuvexa-crm/page.tsx
@@ -1,11 +1,11 @@
-import CRMFeatures from '@/components/nuvexa-crm/CRMFeatures';
-import Faq from '@/components/nuvexa-crm/Faq';
+import CRMFeatures from '@/components/nuvexa-crms/CRMFeatures';
+import Faq from '@/components/nuvexa-crms/Faq';
 import Hero from '@/components/home/Hero';
-import HowItWork from '@/components/nuvexa-crm/HowItWork';
+import HowItWork from '@/components/nuvexa-crms/HowItWork';
 import Integration from '@/components/home/Integration';
 import Pricing from '@/components/home/Pricing';
-import SelectUs from '@/components/nuvexa-crm/SelectUs';
-import Testimonial from '@/components/nuvexa-crm/Testimonial';
+import SelectUs from '@/components/nuvexa-crms/SelectUs';
+import Testimonial from '@/components/nuvexa-crms/Testimonial';
 import CTA from '@/components/shared/cta/CTA';
 import { defaultMetadata } from '@/utils/generateMetaData';
 import { Metadata } from 'next';
